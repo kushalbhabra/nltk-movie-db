25d342867fa90872d94cc475282b1e2b995a7a2f
Dhaval Nandu <dhaval.nandu2013@gmail.com>
2013-03-14 14:44:05 +0530

    Configured Parse to work in general

--- code/parse.py
+++ code/parse.py
@@ -2,8 +2,9 @@
 
 import nltk
 import os
-
-os.environ["MALTPARSERHOME"] = "C:/Users/DHAVAL/Dropbox/BE_project/CODE/Syncing/maltparser-1.7.2/maltparser-1.7.2.jar"
+print os.getcwd()
+os.environ["MALTPARSERHOME"] = os.getcwd()+"\maltparser-1.7.2\\maltparser-1.7.2.jar"
 query = 'Which movies have actors James and Lilly'
-parser = nltk.parse.malt.MaltParser(mco = 'engmalt.linear-1.7')
+parser = nltk.parse.malt.MaltParser(mco = 'engmalt.linear-1.7',
+                                    working_dir = 'C:\Users\DHAVAL\Documents\GitHub\nltk-movie-db\code\maltparser-1.7.2')
 print parser.parse(query)
