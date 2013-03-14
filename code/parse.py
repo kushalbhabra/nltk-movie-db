1b6951ef17de578313598b4f1f511a6bb0b7a5c1
Dhaval Nandu <dhaval.nandu2013@gmail.com>
2013-03-14 16:49:26 +0530

    Finally Settings done

--- code/parse.py
+++ code/parse.py
@@ -1,10 +1,11 @@
 ## Placing MALT parser
-
+import tag
 import nltk
 import os
 print os.getcwd()
 os.environ["MALTPARSERHOME"] = os.getcwd()+"\\maltparser-1.7.2\\maltparser-1.7.2.jar"
 query = 'Which movies have actors James and Lilly'
 parser = nltk.parse.malt.MaltParser(mco = 'engmalt.linear-1.7',
-                                    working_dir = os.getcwd()+"\\maltparser-1.7.2")
-print parser.parse(query)
+                                    working_dir = os.getcwd()+"\\maltparser-1.7.2",
+                                    tagger=tag)
+print parser.parse(query.split())
