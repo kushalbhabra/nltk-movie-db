7b5ce7150777ff23429805584731bd5737edfaac
Dhaval Nandu <dhaval.nandu2013@gmail.com>
2013-04-28 12:34:15 +0530

    act_direct_001

--- code/Actor_Director.py
+++ code/Actor_Director.py
@@ -3,33 +3,35 @@ def dbConnect():
     conn = MySQLdb.connect (host = "localhost", user = "root", passwd = "msdhoni", db = "imdb")
     return conn
 
-def verifyPersonWithDb(pTokens):
+def verifyPersonWithDb(p):
     # Database connection
     ret_tokens=[]
     conn = dbConnect()
     cursorActor = conn.cursor()
     cursorDirector = conn.cursor()
-    for p in pTokens:
-        p = p['value'].title()
-        s = p.split(' ')
-        if len(s)==3:
-            s=[' '.join(s[0:2]),s[2]]
-        if len(s)==2: # Nicholas Cage
-            cursorActor.execute("SELECT * FROM actors WHERE first_name = %s AND last_name = %s", (s[0],s[1]))
-            cursorDirector.execute("SELECT * FROM directors WHERE first_name = %s AND last_name = %s", (s[0],s[1]))
-        else: # Nicholas
-            cursorActor.execute("SELECT * FROM actors WHERE first_name = %s OR last_name = %s", (s[0],s[0]))
-            cursorDirector.execute("SELECT * FROM directors WHERE first_name = %s OR last_name = %s", (s[0],s[0]))
-        rowsActor=cursorActor.fetchall()
-        if rowsActor!=():
-            token = {'token':p,'dbelement':' '.join(s),'attribute':'actorname','explicit':True}
-            ret_tokens.append(token)
-        rowsDirector=cursorDirector.fetchall()
-        if rowsDirector!=():
-            token = {'token':p,'dbelement':' '.join(s),'attribute':'directorname','explicit':True}
-            ret_tokens.append(token)
+
+    s = p.split(' ')
+    if len(s)==3:
+        s=[' '.join(s[0:2]),s[2]]
+    if len(s)==2: # Nicholas Cage
+        cursorActor.execute("SELECT * FROM actor WHERE first_name = %s AND last_name = %s", (s[0],s[1]))
+        cursorDirector.execute("SELECT * FROM director WHERE first_name = %s AND last_name = %s", (s[0],s[1]))
+    else: # Nicholas
+        cursorActor.execute("SELECT * FROM actor WHERE first_name = %s OR last_name = %s", (s[0],s[0]))
+        cursorDirector.execute("SELECT * FROM director WHERE first_name = %s OR last_name = %s", (s[0],s[0]))
+    rowsActor=cursorActor.fetchall()
+    if rowsActor!=():
+        ret_tokens.append('actor')
+    rowsDirector=cursorDirector.fetchall()
+    if rowsDirector!=():
+        ret_tokens.append('director')
             
     conn.close()
-    return ret_tokens
+    print "Here"
+    print ret_tokens
+    if ret_tokens:
+        return ret_tokens
+    else:
+        print "No one in database"
 
-verifyPersonWithDb(['James Franco'])
+verifyPersonWithDb('James Franco')
